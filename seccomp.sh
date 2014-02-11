fa1ccd2f7777b3c0815d68bcaffe63f220359240
Nels Nelson <nels.n.nelson@gmail.com>
2014-02-11 02:25:25 -0600

    Example of lxc-execute
    Illustrates seccomp syscall whitelist failure

---
+++ seccomp.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+cat << EOF > /tmp/config
+lxc.seccomp = /tmp/whitelist
+EOF
+cat << EOF > /tmp/whitelist
+1
+whitelist
+1
+EOF
+echo "Executing lxc instance:"
+sudo lxc-execute -n echo-test -f /tmp/config -l DEBUG -o /tmp/lxc.log -- cat /etc/hostname
+echo "Debug output:"
+cat /tmp/lxc.log
+rm /tmp/config
+rm /tmp/whitelist
+sudo rm /tmp/lxc.log
+
